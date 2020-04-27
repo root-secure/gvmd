1485f4c4447bf19996d93bc4a2de916f448a6a03
aschryver <aaron.schryver@rootsecure.com>
2020-04-27 13:39:38 -0400

    openvas-manager

--- awn_copy_deb.bash
+++ awn_copy_deb.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-DEB_BUILD_DIR=/tmp/gvmd
+GVMD_DEB_BUILD_DIR=/tmp/gvmd
+OPENVAS_MGR_DEB_BUILD_DIR=/tmp/openvas-manager
 IMAGE="$1"
 docker run -it ${IMAGE} /bin/true
 LATEST=$(docker ps -l -f ancestor=${IMAGE} --format "{{.ID}}")
-docker cp ${LATEST}:${DEB_BUILD_DIR}/gvmd_8.0.1_amd64.deb ./
+docker cp ${LATEST}:${GVMD_DEB_BUILD_DIR}/gvmd_8.0.1_amd64.deb ./
+docker cp ${LATEST}:${OPENVAS_MGR_DEB_BUILD_DIR}/openvas-manager_8.0.1_amd64.deb ./
 for dock in $(docker ps -f ancestor=${IMAGE} -a -q) ; do
 	docker rm $dock
 done
